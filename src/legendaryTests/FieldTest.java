414801d545fe0a5fd89c53797f93d4a8685ecdea
Sam Pastoriza <pastorsj@rose-hulman.edu>
2015-12-30 15:01:14 -0500

    Updated gitignore file and added two files for fields and methods

--- src/legendaryTests/FieldTest.java
+++ src/legendaryTests/FieldTest.java
@@ -1,5 +1,46 @@
 package legendaryTests;
 
-public class FieldTest {
+import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import legendaryClasses.Field;
+import legendaryInterfaces.IField;
+
+public class FieldTest {
+	
+	IField field;
+	
+	@Before
+	public void setUp() {
+		this.field = new Field();
+	}
+	
+	@After
+	public void takeDown() {
+		this.field = null;
+	}
+	
+	@Test
+	public void testAccess() {
+		this.field.setAccess("public");
+		String test = this.field.getAccess();
+		assertEquals("public", test);
+	}
+	
+	@Test
+	public void testMethodName() {
+		this.field.setMethodName("TestMethod");
+		String testName = this.field.getMethodName();
+		assertEquals("TestMethod", testName);
+	}
+	
+	@Test
+	public void testReturnType() {
+		this.field.setReturnType("String");
+		String testReturn = this.field.getReturnType();
+		assertEquals("String", testReturn);
+	}
 }
--- src/legendaryTests/MethodTest.java
+++ src/legendaryTests/MethodTest.java
@@ -1,5 +1,60 @@
 package legendaryTests;
 
-public class MethodTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import legendaryClasses.Method;
+import legendaryInterfaces.IMethod;
+
+public class MethodTest {
+	
+	IMethod method;
+	
+	@Before
+	public void setUp() {
+		this.method = new Method();
+	}
+	
+	@After
+	public void takeDown() {
+		this.method = null;
+	}
+	
+	@Test
+	public void testAccess() {
+		this.method.setAccess("private");
+		String access = this.method.getAccess();
+		assertEquals("private", access);
+	}
+	
+	@Test
+	public void testMethodName() {
+		this.method.setMethodName("TestMethod");
+		String testName = this.method.getMethodName();
+		assertEquals("TestMethod", testName);
+	}
+	
+	@Test
+	public void testParameters() {
+		List<String> parameters = new ArrayList<>();
+		parameters.add("String");
+		parameters.add("int");
+		parameters.add("double");
+		this.method.setParameters(parameters);
+		List<String> testParams = this.method.getParameters();
+		assertEquals(parameters, testParams);
+	}
+	
+	@Test
+	public void testReturnType() {
+		this.method.setReturnType("int");
+		String testReturn = this.method.getReturnType();
+		assertEquals("int", testReturn);
+	}
 }
