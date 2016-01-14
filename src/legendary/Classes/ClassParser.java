1696f77d11a7cdfff2192babfab32b0033288625
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-01-14 01:23:06 -0500

    Updated Manually Generated Sequence Diagrams and Auto Gen project code

--- src/legendary/Classes/ClassParser.java
+++ src/legendary/Classes/ClassParser.java
@@ -45,7 +45,7 @@ public class ClassParser {
 		writer.write(builder.toString());
 		writer.close();
 		Runtime rt = Runtime.getRuntime();
-		rt.exec("java -jar ./lib/sdedit-4.2-beta1.jar -o ./input_output/SDEoutput.png -t png ./input_output/text.sd");
+//		rt.exec("java -jar ./lib/sdedit-4.2-beta1.jar -o ./input_output/SDEoutput.png -t png ./input_output/text.sd");
 		//Desktop.getDesktop().open(new File("./input_output/SDEoutput.png"));
 		System.out.println(builder.toString());
 	}
@@ -59,7 +59,7 @@ public class ClassParser {
 		writer.write(builder.toString());
 		writer.close();
 		Runtime rt = Runtime.getRuntime();
-		rt.exec("./lib/Graphviz2.38/bin/dot -Tpng ./input_output/text.dot -o ./input_output/GraphVizoutput.png");
+//		rt.exec("./lib/Graphviz2.38/bin/dot -Tpng ./input_output/text.dot -o ./input_output/GraphVizoutput.png");
 //		Desktop.getDesktop().open(new File("./input_output/GraphVizoutput.png"));
 		// System.out.println(builder.toString());
 	}
